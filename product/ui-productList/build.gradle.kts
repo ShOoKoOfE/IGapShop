7a810a94053a8ff5efb8c2e882271b88862675f5
shokoofe <shokoofe.adeli@gmail.com>
2022-03-10 15:17:18 +0330

    start realm

--- product/ui-productList/build.gradle.kts
+++ product/ui-productList/build.gradle.kts
@@ -1,9 +1,7 @@
 apply {
     from("$rootDir/android-library-build.gradle")
 }
-plugins {
-    kotlin(KotlinPlugins.serialization) version Kotlin.version
-}
+
 dependencies {
     "implementation"(project(Modules.core))
     "implementation"(project(Modules.dataSource))
